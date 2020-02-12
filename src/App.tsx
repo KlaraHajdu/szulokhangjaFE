ab9dc6549f92d945e2f01a6642e6476d678610ec
Koltai Gergely Álmos <kgalmos@gmail.com>
2020-02-12 10:36:25 +0100

    Removed unecessary imports.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import PageTitle from "./components/PageTitle";
 
-
 const App: React.FC = () => {
-  const fetchCsoki = () => {};
-
   return (
     <div className="App">
       <PageTitle />
