6c1e737e24fe86199fbb93657b2ed086b0709896
Koltai Gergely Álmos <kgalmos@gmail.com>
2020-02-12 10:14:01 +0100

    Changed import of logo to use the "require" method instead of manually importing it.

--- src/components/PageTitle.tsx
+++ src/components/PageTitle.tsx
@@ -1,6 +1,5 @@
 import React, { ReactElement } from "react";
 import NavBar from "./NavBar";
-import logo from ".././logo2.png";
 
 interface Props {}
 
@@ -8,16 +7,27 @@ function PageTitle(): ReactElement {
   return (
     <section className="hero is-small is-warning is-bold">
       <div className="hero-body">
-        <img src={logo} className ="App-logo" alt="logo"  style={{width: 100, height: 100, position: 'absolute', right: 60, top:10}}/>
+        <img
+          src={require("../logo2.png")}
+          className="App-logo"
+          alt="logo"
+          style={{
+            width: 100,
+            height: 100,
+            position: "absolute",
+            right: 60,
+            top: 10
+          }}
+        />
         <div className="container">
           <h1 className="title">Szülők hangja</h1>
-            <p>{"\n"}</p>
-            <h6 className="subtitle is-6">
+          <p>{"\n"}</p>
+          <h6 className="subtitle is-6">
             <p>A gyermekeink jobb oktatást, </p>
             <p>a tanárok nagyobb megbecsülést,</p>
             <p>az iskolák több forrást érdemelnek!</p>
           </h6>
-      <NavBar />
+          <NavBar />
         </div>
       </div>
     </section>
