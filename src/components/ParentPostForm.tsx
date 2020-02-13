0eb2e974ad2ba6be875ce082ab42ffc5fdeaf055
HajduKlara <klara@DESKTOP-PDLOF11>
2020-02-13 13:58:43 +0100

    Add info component and form for parent posting

---
+++ src/components/ParentPostForm.tsx
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+import { render } from "@testing-library/react";
+
+//interface Props {}
+
+class ParentPostForm extends React.Component {
+  render() {
+    return (
+      <div className="container-fluid" style={{ margin: 20, padding: 30 }}>
+        <div className="box">
+          <form>
+            <select>
+              <option value="Personal" selected>
+                Személyi feltételekről szeretnék írni
+              </option>
+              <option value="Material">
+                Tárgyi feltételekről szeretnék írni
+              </option>
+            </select>
+            <p>Vélemény </p>
+            <textarea style={{ width: "100%" }} name="message"></textarea>
+            <div>
+              <button className="button is-info">Elküldöm</button>
+            </div>
+          </form>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default ParentPostForm;
