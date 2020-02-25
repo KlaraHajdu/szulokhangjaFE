7289432073c332c841ac5061f0ab08e5b4cb3fa9
Koltai Gergely Álmos <kgalmos@gmail.com>
2020-02-25 14:42:56 +0100

    Map now displays icons for all counties with number of posts.

--- src/components/HungaryCountyMap.jsx
+++ src/components/HungaryCountyMap.jsx
@@ -1,15 +1,46 @@
-import React, { useEffect, useState } from "react";
-import ReactMapGL from "react-map-gl";
-import { mapBoxApiToken } from "../static/util/util";
+import React, { useState, useContext, useEffect } from "react";
+import ReactMapGL, { Marker } from "react-map-gl";
+import { mapBoxApiToken, apiGet, parentPostsRoute } from "../static/util/util";
+import * as counties from "../static/geojson/countiesGeo.json";
+import { ParentPostContext } from "./ParentPostProvider";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSchool } from "@fortawesome/free-solid-svg-icons";
 
 function HungaryCountyMap(props) {
     const [viewPort, setviewPort] = useState({
         latitude: 47.1817585,
         longitude: 19.5060937,
-        width: 1200,
-        height: 600,
+        width: 1600,
+        height: 800,
         zoom: 6.8
     });
+
+    const [parentPosts, setParentPosts] = useContext(ParentPostContext);
+
+    useEffect(() => {
+        const fetch = () => {
+            apiGet(parentPostsRoute + "listall", jsonResponse => {
+                setParentPosts(jsonResponse);
+            });
+        };
+        fetch();
+    }, []);
+
+    const getNumberOfPostForRegion = region => {
+        let numberOfPosts = 0;
+
+        for (let i = 0; i < parentPosts.length; i++) {
+            if (parentPosts[i].location === region) {
+                numberOfPosts++;
+            }
+        }
+
+        if (numberOfPosts > 0) {
+            return `${numberOfPosts} DB`;
+        } else {
+            return;
+        }
+    };
 
     const setZoom = zoom => {
         if (zoom < 6.8) {
@@ -19,22 +50,67 @@ function HungaryCountyMap(props) {
         }
     };
 
+    const setLatitudeBoundaries = (latitude, zoom) => {
+        console.log(zoom);
+        if (zoom > 6.8) {
+            if (latitude > 48.27440611924083) {
+                return 48.27440611924083;
+            } else if (latitude < 45.72973941244668) {
+                return 45.72973941244668;
+            } else {
+                return latitude;
+            }
+        } else {
+            return 47.1817585;
+        }
+    };
+
+    const setLongitudeBoundaries = (longitude, zoom) => {
+        if (zoom > 6.8) {
+            if (longitude < 16.096828856526308) {
+                return 16.096828856526308;
+            } else if (longitude > 22.856966519292534) {
+                return 22.856966519292534;
+            } else {
+                return longitude;
+            }
+        } else {
+            return 19.5060937;
+        }
+    };
+
     return (
         <div className="container">
             <ReactMapGL
                 {...viewPort}
                 mapboxApiAccessToken={mapBoxApiToken}
-                mapStyle="mapbox://styles/hexxar/ck70lone822yy1iqanwflyh3e"
+                mapStyle="mapbox://styles/hexxar/ck71sd4cq07w31iqz9wd81650"
                 onViewportChange={viewPort => {
                     setviewPort({
-                        latitude: 47.1817585,
-                        longitude: 19.5060937,
+                        latitude: setLatitudeBoundaries(viewPort.latitude, viewPort.zoom),
+                        longitude: setLongitudeBoundaries(viewPort.longitude, viewPort.zoom),
                         width: 1200,
                         height: 600,
                         zoom: setZoom(viewPort.zoom)
                     });
                 }}
-            ></ReactMapGL>
+            >
+                {parentPosts &&
+                    counties.features.map(county => (
+                        <Marker
+                            key={county.properties.id}
+                            latitude={county.geometry.coordinates[1]}
+                            longitude={county.geometry.coordinates[0]}
+                        >
+                            <a className="icon has-text-danger is-large">
+                                <FontAwesomeIcon icon={faSchool} size="2x" title="ASD"></FontAwesomeIcon>
+                            </a>
+                            <span className="has-text-danger is-large is-bold">
+                                {getNumberOfPostForRegion(county.properties.name)}
+                            </span>
+                        </Marker>
+                    ))}
+            </ReactMapGL>
         </div>
     );
 }
--- src/components/PageTitle.tsx
+++ src/components/PageTitle.tsx
@@ -5,32 +5,25 @@ import { Link } from "react-router-dom";
 interface Props {}
 
 function PageTitle(): ReactElement {
-  return (
-    <section className="hero is-small is-warning is-bold">
-      <div className="hero-body">
-        <div className="tile is-parent">
-          <Link to="/" className="tile is-child is-1">
-            <figure className="image is-96x96">
-              <img src={require("../logo2.png")} alt="logo" />
-            </figure>
-          </Link>
-          <div className="tile is-3 is-child">
-            <h1 className="title is-size-2 has-text-weight-bold">
-              Szülők hangja
-            </h1>
-            <h6 className="subtitle is-size-6">
-              <p>A gyermekeink jobb oktatást, </p>
-              <p>a tanárok nagyobb megbecsülést,</p>
-              <p>az iskolák több forrást érdemelnek!</p>
-            </h6>
-          </div>
-        </div>
-        <div className="hero-bottom">
-          <NavBar />
-        </div>
-      </div>
-    </section>
-  );
+    return (
+        <section className="hero is-small is-warning is-bold">
+            <div className="hero-body">
+                <div className="tile is-parent">
+                    <Link to="/" className="tile is-child is-1">
+                        <figure className="image is-64x64">
+                            <img src={require("../logo2.png")} alt="logo" />
+                        </figure>
+                    </Link>
+                    <div className="tile is-3 is-child">
+                        <h1 className="title is-size-2 has-text-weight-bold">Szülők hangja</h1>
+                    </div>
+                </div>
+                <div className="hero-bottom">
+                    <NavBar />
+                </div>
+            </div>
+        </section>
+    );
 }
 
 export default PageTitle;
