8729b206dec2603f591098a2e2561619dc2eb11e
KlaraHajdu <49884249+KlaraHajdu@users.noreply.github.com>
2020-02-25 19:09:59 +0100

    Delet paper from style

--- src/components/FilterPosts.tsx
+++ src/components/FilterPosts.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { makeStyles, createStyles, Theme } from "@material-ui/core/styles";
 import Typography from "@material-ui/core/Typography";
-import Paper from "@material-ui/core/Paper";
 import Divider from "@material-ui/core/Divider";
 import Grid from "@material-ui/core/Grid";
 import Switch from "@material-ui/core/Switch";
@@ -12,16 +11,7 @@ import Checkbox from "@material-ui/core/Checkbox";
 const useStyles = makeStyles((theme: Theme) =>
     createStyles({
         container: {
-            display: "grid",
-            gridTemplateColumns: "repeat(12, 1fr)",
-            gridGap: theme.spacing(3)
-        },
-        paper: {
-            padding: theme.spacing(1),
-            textAlign: "center",
-            color: theme.palette.text.secondary,
-            whiteSpace: "nowrap",
-            marginBottom: theme.spacing(1)
+            margin: "0 0 0 10px"
         },
         divider: {
             margin: theme.spacing(2, 0)
@@ -40,13 +30,13 @@ const FilterPosts: React.FC<Props> = () => {
         IsTeacherSelected: false
     });
 
-    const [positive, setPositive] = React.useState(false);
+    const [positive, setPositive] = React.useState(true);
 
     const [negative, setNegative] = React.useState(false);
 
     const [material, setMaterial] = React.useState(false);
 
-    const [personal, setPersonal] = React.useState(false);
+    const [personal, setPersonal] = React.useState(true);
 
     const classes = useStyles();
 
@@ -104,78 +94,74 @@ const FilterPosts: React.FC<Props> = () => {
     };
     return (
         <div>
-            <Grid container spacing={3}>
+            <Grid container spacing={3} className={classes.container}>
                 <Grid item lg={2}>
-                    <Paper className={classes.paper}>
-                        <FormGroup row>
-                            <FormControlLabel
-                                control={
-                                    <Switch
-                                        checked={parent.IsParentSelected}
-                                        onChange={handleParentChange()}
-                                        value="IsParentSelected"
-                                    />
-                                }
-                                label="Szülők posztjai"
-                            />
-                            <FormControlLabel
-                                control={
-                                    <Switch
-                                        checked={teacher.IsTeacherSelected}
-                                        onChange={handleTeacherChange()}
-                                        value="IsTeacherSelected"
-                                    />
-                                }
-                                label="Tanárok posztjai"
-                            />
-                            <FormControlLabel
-                                control={
-                                    <Checkbox
-                                        defaultChecked={positive}
-                                        onChange={handleChangeIsPositiveSelected}
-                                        value="IsPositive"
-                                        id="PositiveCheckBox"
-                                    />
-                                }
-                                label="Dícsérő posztok"
-                            />
-                            <FormControlLabel
-                                control={
-                                    <Checkbox
-                                        checked={negative}
-                                        onChange={handleChangeIsNegativeSelected}
-                                        id="NegativeCheckBox"
-                                        value="IsNegative"
-                                    />
-                                }
-                                label="Panaszkodó posztok"
-                            />
-                            <FormControlLabel
-                                control={
-                                    <Checkbox
-                                        checked={personal}
-                                        onChange={handleChangeIsPersonalSelected}
-                                        value="IsPersonal"
-                                    />
-                                }
-                                label="Személyi feltételek"
-                            />
-                            <FormControlLabel
-                                control={
-                                    <Checkbox
-                                        checked={material}
-                                        onChange={handleChangeIsMaterialSelected}
-                                        value="IsMaterial"
-                                    />
-                                }
-                                label="Tárgyi feltételek"
-                            />
-                        </FormGroup>
-                    </Paper>
+                    <FormGroup row>
+                        <FormControlLabel
+                            control={
+                                <Switch
+                                    checked={parent.IsParentSelected}
+                                    onChange={handleParentChange()}
+                                    value="IsParentSelected"
+                                />
+                            }
+                            label="Szülők posztjai"
+                        />
+                        <FormControlLabel
+                            control={
+                                <Switch
+                                    checked={teacher.IsTeacherSelected}
+                                    onChange={handleTeacherChange()}
+                                    value="IsTeacherSelected"
+                                />
+                            }
+                            label="Tanárok posztjai"
+                        />
+                        <FormControlLabel
+                            control={
+                                <Checkbox
+                                    defaultChecked={positive}
+                                    onChange={handleChangeIsPositiveSelected}
+                                    value="IsPositive"
+                                    id="PositiveCheckBox"
+                                />
+                            }
+                            label="Dícsérő posztok"
+                        />
+                        <FormControlLabel
+                            control={
+                                <Checkbox
+                                    checked={negative}
+                                    onChange={handleChangeIsNegativeSelected}
+                                    id="NegativeCheckBox"
+                                    value="IsNegative"
+                                />
+                            }
+                            label="Panaszkodó posztok"
+                        />
+                        <FormControlLabel
+                            control={
+                                <Checkbox
+                                    checked={personal}
+                                    onChange={handleChangeIsPersonalSelected}
+                                    value="IsPersonal"
+                                />
+                            }
+                            label="Személyi feltételek"
+                        />
+                        <FormControlLabel
+                            control={
+                                <Checkbox
+                                    checked={material}
+                                    onChange={handleChangeIsMaterialSelected}
+                                    value="IsMaterial"
+                                />
+                            }
+                            label="Tárgyi feltételek"
+                        />
+                    </FormGroup>
                 </Grid>
-                <Grid item lg={10}>
-                    <Paper className={classes.paper}>xs=4</Paper>
-                </Grid>
+                <Grid item lg={10}></Grid>
             </Grid>
         </div>
     );
