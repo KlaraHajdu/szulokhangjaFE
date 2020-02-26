677d316356e0333a5e6c7556a44d943e5b0e36d8
KlaraHajdu <49884249+KlaraHajdu@users.noreply.github.com>
2020-02-26 21:05:55 +0100

    Filter teacherpostlist on FE

--- src/components/TeacherPosts.tsx
+++ src/components/TeacherPosts.tsx
@@ -1,15 +1,18 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import { apiGet, teacherRecPostsRoute, teacherSalPostsRoute } from "../static/util/util";
 import { TeacherSalary, TeacherRecommendation } from "../static/util/dataInterfaces";
 import TeacherSalaryTile from "./TeacherSalaryTile";
 import TeacherRecommendationTile from "./TeacherRecommendationTile";
 import Typography from "@material-ui/core/Typography";
+import { TeacherFilterContext } from "./TeacherFilterProvider";
 
 interface Props {}
 
-const TeacherPosts: React.FC<Props> = (props) => {
+const TeacherPosts: React.FC<Props> = props => {
     const [teacherSalaries, setteacherSalaries] = useState();
     const [teacherRecommendations, setteacherRecommendations] = useState();
+
+    const [teacherFilters, setTeacherFilters] = useContext(TeacherFilterContext);
 
     useEffect(() => {
         const fetch = (): void => {
@@ -23,32 +26,27 @@ const TeacherPosts: React.FC<Props> = (props) => {
         fetch();
     }, []);
 
+    let allTypePosts: any = [];
+
+    for (let key in teacherSalaries) {
+        if (teacherFilters.salary === true) allTypePosts.push(teacherSalaries[key]);
+    }
+
+    for (let key in teacherRecommendations) {
+        if (teacherFilters.recommendation === true) allTypePosts.push(teacherRecommendations[key]);
+    }
+
     return (
-        <div>
-            <Typography variant="h5" gutterBottom>
-                Tanárok mondták
-            </Typography>
-            <div className="tile is-parent">
-                <div className="tile is-child">
-                    <Typography variant="h6" gutterBottom>
-                        Javaslatok
-                    </Typography>
-                    {teacherRecommendations &&
-                        teacherRecommendations.map((recommendation: TeacherRecommendation) => {
-                            return <TeacherRecommendationTile key={recommendation.id} post={recommendation} />;
-                        })}
-                </div>
-                <div className="tile is-child">
-                    <Typography variant="h6" gutterBottom>
-                        Anyagi megbecsülés{" "}
-                    </Typography>
-                    {teacherSalaries &&
-                        teacherSalaries.map((teacherSalary: TeacherSalary) => {
-                            return <TeacherSalaryTile key={teacherSalary.id} post={teacherSalary} />;
-                        })}
-                </div>
-            </div>
-        </div>
+        <React.Fragment>
+            <Typography variant="h5">Tanárok mondták</Typography>
+            {teacherRecommendations &&
+                teacherSalaries &&
+                allTypePosts.map((post: any) => {
+                    if (post.salary) {
+                        return <TeacherSalaryTile key={post.id} post={post} />;
+                    } else return <TeacherRecommendationTile key={post.id} post={post} />;
+                })}
+        </React.Fragment>
     );
 };
 
