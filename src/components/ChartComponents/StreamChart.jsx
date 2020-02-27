0d4a482a049af2a44290df7d7a31a14cd497ce52
KlaraHajdu <49884249+KlaraHajdu@users.noreply.github.com>
2020-02-27 13:28:00 +0100

    Legends for charts adjusted

--- src/components/ChartComponents/StreamChart.jsx
+++ src/components/ChartComponents/StreamChart.jsx
@@ -6,95 +6,95 @@ import { ResponsiveStream } from "@nivo/stream";
 // website examples showcase many properties,
 // you'll often use just a few of them.
 const StreamChart = ({ data /* see data tab */ }) => (
-  <ResponsiveStream
-    data={data[0]}
-    keys={data[1]}
-    margin={{ top: 50, right: 110, bottom: 50, left: 60 }}
-    axisTop={null}
-    axisRight={null}
-    axisBottom={{
-      orient: "bottom",
-      tickSize: 5,
-      tickPadding: 5,
-      tickRotation: 0,
-      legend: "Posztok alakulasa idovel helysegkent",
-      legendOffset: 35
-    }}
-    axisLeft={{
-      orient: "left",
-      tickSize: 5,
-      tickPadding: 5,
-      tickRotation: 0,
-      legend: "",
-      legendOffset: -40
-    }}
-    curve="basis"
-    offsetType="diverging"
-    colors={{ scheme: "nivo" }}
-    fillOpacity={0.85}
-    borderColor={{ theme: "background" }}
-    defs={[
-      {
-        id: "dots",
-        type: "patternDots",
-        background: "inherit",
-        color: "#2c998f",
-        size: 4,
-        padding: 2,
-        stagger: true
-      },
-      {
-        id: "squares",
-        type: "patternSquares",
-        background: "inherit",
-        color: "#e4c912",
-        size: 6,
-        padding: 2,
-        stagger: true
-      }
-    ]}
-    fill={[
-      {
-        match: {
-          id: "Paul"
-        },
-        id: "dots"
-      },
-      {
-        match: {
-          id: "Marcel"
-        },
-        id: "squares"
-      }
-    ]}
-    dotSize={8}
-    dotColor={{ from: "color" }}
-    dotBorderWidth={2}
-    dotBorderColor={{ from: "color", modifiers: [["darker", 0.7]] }}
-    animate={true}
-    motionStiffness={90}
-    motionDamping={15}
-    legends={[
-      {
-        anchor: "bottom-right",
-        direction: "column",
-        translateX: 100,
-        itemWidth: 80,
-        itemHeight: 20,
-        itemTextColor: "#999999",
-        symbolSize: 12,
-        symbolShape: "circle",
-        effects: [
-          {
-            on: "hover",
-            style: {
-              itemTextColor: "#000000"
+    <ResponsiveStream
+        data={data[0]}
+        keys={data[1]}
+        margin={{ top: 50, right: 110, bottom: 50, left: 60 }}
+        axisTop={null}
+        axisRight={null}
+        axisBottom={{
+            orient: "bottom",
+            tickSize: 5,
+            tickPadding: 5,
+            tickRotation: 0,
+            legend: "Posztok alakulása idővel megyénként",
+            legendOffset: 35
+        }}
+        axisLeft={{
+            orient: "left",
+            tickSize: 5,
+            tickPadding: 5,
+            tickRotation: 0,
+            legend: "",
+            legendOffset: -40
+        }}
+        curve="basis"
+        offsetType="diverging"
+        colors={{ scheme: "nivo" }}
+        fillOpacity={0.85}
+        borderColor={{ theme: "background" }}
+        defs={[
+            {
+                id: "dots",
+                type: "patternDots",
+                background: "inherit",
+                color: "#2c998f",
+                size: 4,
+                padding: 2,
+                stagger: true
+            },
+            {
+                id: "squares",
+                type: "patternSquares",
+                background: "inherit",
+                color: "#e4c912",
+                size: 6,
+                padding: 2,
+                stagger: true
             }
-          }
-        ]
-      }
-    ]}
-  />
+        ]}
+        fill={[
+            {
+                match: {
+                    id: "Paul"
+                },
+                id: "dots"
+            },
+            {
+                match: {
+                    id: "Marcel"
+                },
+                id: "squares"
+            }
+        ]}
+        dotSize={8}
+        dotColor={{ from: "color" }}
+        dotBorderWidth={2}
+        dotBorderColor={{ from: "color", modifiers: [["darker", 0.7]] }}
+        animate={true}
+        motionStiffness={90}
+        motionDamping={15}
+        legends={[
+            {
+                anchor: "bottom-right",
+                direction: "column",
+                translateX: 100,
+                itemWidth: 80,
+                itemHeight: 20,
+                itemTextColor: "#999999",
+                symbolSize: 12,
+                symbolShape: "circle",
+                effects: [
+                    {
+                        on: "hover",
+                        style: {
+                            itemTextColor: "#000000"
+                        }
+                    }
+                ]
+            }
+        ]}
+    />
 );
 
 export default StreamChart;
--- src/components/Statistics.jsx
+++ src/components/Statistics.jsx
@@ -2,74 +2,81 @@ import React, { useEffect, useContext, useState } from "react";
 import StreamChart from "./ChartComponents/StreamChart";
 import DateBarChart from "./ChartComponents/DateBarChart";
 import { Link, useParams } from "react-router-dom";
-import {
-  apiGet,
-  parentPostsRoute,
-  teacherRecPostsRoute,
-  teacherSalPostsRoute
-} from "../static/util/util";
+import { apiGet, parentPostsRoute, teacherRecPostsRoute, teacherSalPostsRoute } from "../static/util/util";
 import { dataMaker } from "../static/util/dataFormatter";
-import {Button, ButtonGroup, Container} from '@material-ui/core'
+import { Button, ButtonGroup, Container } from "@material-ui/core";
 
 function Statistics() {
-  const { id } = useParams();
+    const { id } = useParams();
 
-  const [teacherSalaries, setteacherSalaries] = useState([]);
-  const [teacherRecommendations, setteacherRecommendations] = useState([]);
-  const [parentPst, setParentPst] = useState([]);
+    const [teacherSalaries, setteacherSalaries] = useState([]);
+    const [teacherRecommendations, setteacherRecommendations] = useState([]);
+    const [parentPst, setParentPst] = useState([]);
 
-  // Promise.all([getch url 1, fetch url 2, fetch url 3]).then(function(values) {
-  //  values.json .... / process
-  // });
+    // Promise.all([getch url 1, fetch url 2, fetch url 3]).then(function(values) {
+    //  values.json .... / process
+    // });
 
-  useEffect(() => {
-    const fetch = () => {
-      apiGet(teacherSalPostsRoute + "listall", jsonresponse => {
-        setteacherSalaries(jsonresponse);
-      });
-      apiGet(teacherRecPostsRoute + "listall", jsonresponse => {
-        setteacherRecommendations(jsonresponse);
-      });
-      apiGet(parentPostsRoute + "listall", jsonresponse => {
-        setParentPst(jsonresponse);
-      });
+    useEffect(() => {
+        const fetch = () => {
+            apiGet(teacherSalPostsRoute + "listall", jsonresponse => {
+                setteacherSalaries(jsonresponse);
+            });
+            apiGet(teacherRecPostsRoute + "listall", jsonresponse => {
+                setteacherRecommendations(jsonresponse);
+            });
+            apiGet(parentPostsRoute + "listall", jsonresponse => {
+                setParentPst(jsonresponse);
+            });
+        };
+        fetch();
+    }, []);
+
+    let total = [...teacherSalaries, ...teacherRecommendations, ...parentPst];
+
+    dataMaker("3", total);
+
+    let renderStat = id => {
+        switch (id) {
+            case "1":
+                return <StreamChart data={dataMaker("1", total)}></StreamChart>;
+                break;
+            case "2":
+                return <DateBarChart data={dataMaker("2", total)}></DateBarChart>;
+                break;
+            case "3":
+                return <DateBarChart data={dataMaker("3", total)}></DateBarChart>;
+                break;
+            default:
+                return null;
+        }
     };
-    fetch();
-  }, []);
 
-  let total = [...teacherSalaries, ...teacherRecommendations, ...parentPst];
+    return (
+        <React.Fragment>
+            <ButtonGroup>
+                <Link to="/statistics/1">
+                    <Button size="large" color="primary">
+                        Idővonal
+                    </Button>
+                </Link>
+                <Link to="/statistics/2">
+                    <Button size="large" color="primary">
+                        Hely szerint
+                    </Button>
+                </Link>
+                <Link to="/statistics/3">
+                    <Button size="large" color="primary">
+                        Idő szerint
+                    </Button>
+                </Link>
+            </ButtonGroup>
 
-  dataMaker("3", total);
-
-  let renderStat = id => {
-    switch (id) {
-      case "1":
-        return <StreamChart data={dataMaker("1", total)}></StreamChart>;
-        break;
-      case "2":
-        return <DateBarChart data={dataMaker("2", total)}></DateBarChart>;
-        break;
-      case "3":
-        return <DateBarChart data={dataMaker("3", total)}></DateBarChart>;
-        break;
-      default:
-        return null;
-    }
-  };
-
-  return (
-    <React.Fragment>
-        <ButtonGroup>
-              <Link to="/statistics/1"><Button size="large"  color="primary">Idovonal</Button></Link> 
-              <Link to="/statistics/2"><Button size="large"  color="primary">Hely szerint</Button></Link>
-              <Link to="/statistics/3"><Button size="large"  color="primary">Ido szerint</Button></Link>
-        </ButtonGroup>
-
-      <Container maxWidth="lg" style={{height:"70vh"}}>
-        {renderStat(id)}
-      </Container>
-    </React.Fragment>
-  );
+            <Container maxWidth="lg" style={{ height: "70vh" }}>
+                {renderStat(id)}
+            </Container>
+        </React.Fragment>
+    );
 }
 
 export default Statistics;
