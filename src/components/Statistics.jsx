5891f98b4e0aef295fbe8200c845811acc602d58
Daniel Zsk <dzsukov@gmail.com>
2020-02-27 02:04:44 +0100

    minor fixed and prettyfication

--- src/components/Statistics.jsx
+++ src/components/Statistics.jsx
@@ -11,131 +11,46 @@ import {
 } from "../static/util/util";
 import { dataMaker } from "../static/util/dataFormatter";
 
-
-let testdata = [
-  {
-    Raoul: 64,
-    Josiane: 141,
-    Marcel: 174,
-    René: 34,
-    Paul: 58,
-    Jacques: 167
-  },
-  {
-    Raoul: 135,
-    Josiane: 106,
-    Marcel: 184,
-    René: 26,
-    Paul: 194,
-    Jacques: 68
-  },
-  {
-    Raoul: 195,
-    Josiane: 58,
-    Marcel: 111,
-    René: 19,
-    Paul: 52,
-    Jacques: 90
-  },
-  {
-    Raoul: 148,
-    Josiane: 11,
-    Marcel: 175,
-    René: 15,
-    Paul: 148,
-    Jacques: 36
-  },
-  {
-    Raoul: 160,
-    Josiane: 185,
-    Marcel: 105,
-    René: 33,
-    Paul: 46,
-    Jacques: 133
-  },
-  {
-    Raoul: 91,
-    Josiane: 127,
-    Marcel: 109,
-    René: 77,
-    Paul: 111,
-    Jacques: 34
-  },
-  {
-    Raoul: 45,
-    Josiane: 155,
-    Marcel: 45,
-    René: 110,
-    Paul: 169,
-    Jacques: 70
-  },
-  {
-    Raoul: 122,
-    Josiane: 21,
-    Marcel: 137,
-    René: 125,
-    Paul: 155,
-    Jacques: 110
-  },
-  {
-    Raoul: 173,
-    Josiane: 113,
-    Marcel: 97,
-    René: 183,
-    Paul: 188,
-    Jacques: 30
-  }
-];
-
-
 function Statistics() {
-
-
   const { id } = useParams();
 
   const [teacherSalaries, setteacherSalaries] = useState([]);
   const [teacherRecommendations, setteacherRecommendations] = useState([]);
   const [parentPst, setParentPst] = useState([]);
 
-
   // Promise.all([getch url 1, fetch url 2, fetch url 3]).then(function(values) {
   //  values.json .... / process
   // });
-
 
   useEffect(() => {
     const fetch = () => {
       apiGet(teacherSalPostsRoute + "listall", jsonresponse => {
         setteacherSalaries(jsonresponse);
-
       });
       apiGet(teacherRecPostsRoute + "listall", jsonresponse => {
         setteacherRecommendations(jsonresponse);
-
       });
       apiGet(parentPostsRoute + "listall", jsonresponse => {
         setParentPst(jsonresponse);
-
       });
     };
     fetch();
   }, []);
 
-  let total = [...teacherSalaries,...teacherRecommendations,...parentPst]
-  console.log(dataMaker("1",total))
+  let total = [...teacherSalaries, ...teacherRecommendations, ...parentPst];
 
-  dataMaker("3",total)
+  dataMaker("3", total);
 
   let renderStat = id => {
     switch (id) {
       case "1":
-        return <StreamChart data={dataMaker("1",total)}></StreamChart>;
+        return <StreamChart data={dataMaker("1", total)}></StreamChart>;
         break;
       case "2":
-        return <DateBarChart data={dataMaker("2",total)}></DateBarChart>;
+        return <DateBarChart data={dataMaker("2", total)}></DateBarChart>;
         break;
       case "3":
-        return <DateBarChart data={dataMaker("3",total)}></DateBarChart>
+        return <DateBarChart data={dataMaker("3", total)}></DateBarChart>;
         break;
       default:
         return null;
